c1bead23a64bca456cf2c0ad36714e906f326b3d
Nicksonder Examar <nextgendev12@gmail.com>
2021-09-09 03:40:41 -0400

    Added local Font Awesome.

--- pages/_document.js
+++ pages/_document.js
@@ -21,12 +21,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700&display=swap"
             rel="stylesheet"
           />
-          <link
-            rel="stylesheet"
-            href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-            integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
-            crossOrigin="anonymous"
-          />
+          <link rel="stylesheet" href="/fontaw/css/all.min.css" />
         </Head>
 
         <body>
